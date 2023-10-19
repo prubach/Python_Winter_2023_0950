a85b711fb402a6d6769546a2ff51bb759b48b3ef
Paweł Rubach <prubac@sgh.waw.pl>
2023-10-19 10:12:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+'''
+
+This is a multiline comment
+
+
+'''
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Hi, {}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+a = 5
+if a == 5:
+    print_hi('PyCharm')
+print_hi(3635636)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

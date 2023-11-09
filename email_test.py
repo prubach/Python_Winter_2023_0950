8fc685bb8272c8d8ee3ebdb7010b47b25cc29603
prubac <pawel.rubach@gmail.com>
2023-11-09 10:57:11 +0100

    Email exercise - partially done

--- email_test.py
+++ email_test.py
@@ -2,11 +2,22 @@ email1 = 'pawel.rubach@sgh.waw.pl'
 email2 = '@sgh.waw.pl'
 email3 = 'pawel@pl'
 email4 = 'pawel@'
-email5 = 'pawel@.pl'
+email5 = 'pawel@edeh.'
+email6 = 'pawel@.pl'
+
+email_list = [email1, email2, email3, email4, email5, email6]
 
 #TODO create a function that will test if the email is valid
 def validate_email(email):
+    #if email[-1] == '.' or email[-1] == '@':
+    if email[-1] in ['.', '@']:
+        return False
+    #email[-1]
     #....
     return True
 
-print(validate_email(email1))
+#print(email6[-1])
+
+for em in email_list:
+    print('{}: {}'.format(em, validate_email(em)))
+#print(validate_email(email1))

7e75abf0f4c2dc723c49bc376ba34ebe90814319
prubac <pawel.rubach@gmail.com>
2023-11-16 10:16:01 +0100

    Lists and sets

--- play_with_numbers.py
+++ play_with_numbers.py
@@ -1,9 +1,20 @@
-num_list = [2, 5, 7, 9, 11, 13, 18]
+num_list = [2, 13, 9, 7, 11, 5, 18]
 
 '''
 TODO 
 1. Print the subsequent 2nd powers of elements in the list
 2. Sum the 2nd powers of elements of the list
 '''
+num_list_2nd_pow = [x*x for x in num_list]
+print(num_list_2nd_pow)
+num_list_2nd_pow_sorted = sorted(num_list_2nd_pow)
+num_list_2nd_pow_sorted = sorted(num_list_2nd_pow, reverse=True)
+print(num_list_2nd_pow_sorted)
+#sum(num_list_2nd_pow)
+print('------ set --------')
+set_of_nums = set(num_list_2nd_pow_sorted)
+print(set_of_nums)
+
+
 
 

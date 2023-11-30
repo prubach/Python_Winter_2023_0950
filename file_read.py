c0432f0a26e0fa1bd159034ecb5b352f9d29cf13
prubac <pawel.rubach@gmail.com>
2023-11-30 11:03:04 +0100

    Read txt file

---
+++ file_read.py
@@ -0,0 +1,16 @@
+my_file = 'hello.txt'
+
+# f = open(my_file, 'r')
+# lines = f.readlines()
+# i = 0
+# for line in lines:
+#     i += 1
+#     print(f'{i}: {line}', end='')
+# f.close()
+
+with open(my_file, 'r') as f:
+    lines = f.readlines()
+    i = 0
+    for line in lines:
+        i += 1
+        print(f'{i}: {line}', end='')
